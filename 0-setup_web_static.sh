ccdc7ffdbd1eb049543f99f61c7a3d4505301dc9
Nobleman20 <emmanueloluyege@gmail.com>
2023-05-08 01:28:21 +0000

    0fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,51 +1,23 @@
 #!/usr/bin/env bash
-# A script that sets up my web servers for the deployment of web_static
 
-echo -e "\e[1;32m START\e[0m"
+# script that  Installs Nginx if not installed
+#+ script that  Creates folders /data/web_static/shared
+#+	and /adta/releases/test if not exists
+#+ /data/web_static/current linked to
+#+ /data/web_static/releases/test/
+#+ Creates an /data/web_static/releases/test/index.html
+#+ Configures Nginx to serve /data/web_static/current/ 
+#+	to hbnb_static
 
-#--Updating the packages
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-echo -e "\e[1;32m Packages updated\e[0m"
-echo
-
-#--configure firewall
-sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
-echo
-
-#--created the dir
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "\e[1;32m directories created"
-echo
 
-#--adds test string
-echo "<h1>Welcome to www.smartcodez.tech</h1>" > /data/web_static/releases/test/index.html
-echo -e "\e[1;32m Test string added\e[0m"
-echo
-
-#--prevent overwrite
-if [ -d "/data/web_static/current" ];
-then
-	    echo "path /data/web_static/current exists"
-	        sudo rm -rf /data/web_static/current;
-fi;
-echo -e "\e[1;32m prevent overwrite\e[0m"
-echo
-
-#--create symbolic link
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
 
-
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-echo -e "\e[1;32m Symbolic link created\e[0m"
-echo
-
-
-#--restart NGINX
-sudo service nginx restart
-echo -e "\e[1;32m restart NGINX\e[0m"
+sudo service nginx start
